f3869a2c4548daa33236ab44c32202b086389ee7
felprangel <feliperangel897@gmail.com>
2026-04-22 17:13:10 -0300

    chore: initialize Go project with empty CLI skeleton

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/felipe/qrcode-generator
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
+}
+
+func run(args []string, stdout, stderr io.Writer) int {
+	// routing to be implemented in later tasks
+	return 0
+}
